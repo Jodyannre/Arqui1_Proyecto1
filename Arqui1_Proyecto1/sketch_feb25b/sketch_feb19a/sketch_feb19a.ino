aaf4999fda3ed0e738d6a6848fbfc27eb527c6aa
JavierAlfaroGT <69096882+JavierAlfaroGT@users.noreply.github.com>
2021-02-26 19:48:18 -0600

    reloj y sensor de temperatura

--- Arqui1_Proyecto1/sketch_feb25b/sketch_feb19a/sketch_feb19a.ino
+++ Arqui1_Proyecto1/sketch_feb25b/sketch_feb19a/sketch_feb19a.ino
@@ -1,9 +1,13 @@
 void setup() {
-  // put your setup code here, to run once:
+
   Serial1.begin(9600);
   DDRK = B11111111;
   DDRL = B11111111;
   DDRC = B11111111;
+  pinMode(A0, INPUT);//javier temperatura
+  pinMode(A1, OUTPUT);//javier temperatura led green
+  pinMode(A2, OUTPUT);//javier temperatura led yellow
+  pinMode(A3, OUTPUT);//javier temperatura led red
 
 }
 
@@ -13,11 +17,43 @@ void loop() {
 
 
 
-  tiempo();//verifica los seg,dec,min
+  tiempo();//verifica los seg,dec,min de los 3 display hace la magia de timer
+
+  temperatura();// verifica la temperatura y enciende los leds respectivos
   delay(1000);//obligatorio sin esto el reloj iria mal
 
 
 
+
+
+}
+
+void temperatura() {
+  int val = analogRead(A0);//leo en A1
+  int porcentaje = map(val, 21, 359, -40, 124);//lo paso a porcentaje
+
+  Serial1.print("temperatura: ");
+  Serial1.print(porcentaje);
+  Serial1.println();
+  
+  if (porcentaje < 37) {
+    // Serial1.println("Led verde");
+    digitalWrite(A1, HIGH);
+    digitalWrite(A2, LOW);
+    digitalWrite(A3, LOW);
+  } else if (porcentaje > 36 && porcentaje < 46) {
+    // Serial1.println("Led amarilla");
+    digitalWrite(A1, LOW);
+    digitalWrite(A2, HIGH);
+    digitalWrite(A3, LOW);
+    // en lcd imprimir warning
+  } else {
+    // Serial1.println("Led rojo");
+    digitalWrite(A1, LOW );
+    digitalWrite(A2, LOW);
+    digitalWrite(A3, HIGH);
+    // en lcd imprimir ERROR
+  }
 
 
 }
@@ -30,41 +66,41 @@ bool minOn = false;
 //*****************************************
 void tiempo() {
 
-//calcula las decimas de segundo 0-5
+  //calcula las decimas de segundo 0-5
   if (decOn == true) {
     switch (decimas) {
       case 0 :
-        Serial1.println("0 dec!" );
+        //Serial1.println("0 dec!" );
         decimas++;
         PORTL = B11111100;//print 0 in led
         decOn = false;
         break;
       case 1 :
-        Serial1.println("1 dec" );
+        //Serial1.println("1 dec" );
         decimas++;
         PORTL = B01100000;//print 1 in led
         decOn = false;
         break;
       case 2 :
-        Serial1.println("2 dec" );
+        //Serial1.println("2 dec" );
         decimas++;
         PORTL = B11011010;//print 2 in led
         decOn = false;
         break;
       case 3 :
-        Serial1.println("3 dec" );
+        //Serial1.println("3 dec" );
         decimas++;
         PORTL = B11110010;//print 3 in led
         decOn = false;
         break;
       case 4 :
-        Serial1.println("4 dec" );
+        //Serial1.println("4 dec" );
         decimas++;
         PORTL = B01100110;//print 4 in led
         decOn = false;
         break;
       case 5 :
-        Serial1.println("5 dec" );
+        //Serial1.println("5 dec" );
         decimas = 0;
         PORTL = B10110110;//print 5 in led
         decOn = false;
@@ -75,7 +111,7 @@ void tiempo() {
 
   }
 
-//calcula los min 0-9
+  //calcula los min 0-9
   if (minOn == true) {
     switch (minuto) {
 
@@ -206,7 +242,7 @@ void tiempo() {
       if (decimas == 0 && minuto == 10) {
         minuto = 0;
         PORTC = B11111100;//print 0 in led
-        
+
       }
       break;
     default :
