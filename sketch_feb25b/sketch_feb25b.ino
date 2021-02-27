25945a0459ac144d08e4513130d140a6e48946bc
Jody <jers_033@hotmail.com>
2021-02-27 15:45:45 -0600

    Lectura de datos
    -Lectura de datos desde el puerto serial
    -Pintado de matrices

--- sketch_feb25b/sketch_feb25b.ino
+++ sketch_feb25b/sketch_feb25b.ino
@@ -1,79 +1,218 @@
-String hola;
-String hola1;
-String hola2;
-String hola3;
-String hola4;
-String hola5;
-String hola6;
-String hola7;
-String hola8;
-String hola9;
-String hola54;
-String hola11;
-String hola21;
-String hola31;
-String hola41;
-String hola51;
-String hola61;
-String hola71;
-String hola81;
-String hola91;
-String hola58;
-String hola12;
-String hola22;
-String hola32;
-String hola42;
-String hola52;
-String hola62;
-String hola72;
-String hola82;
-String hola92;
-String hola57;
-String hola13;
-String hola23;
-String hola33;
-String hola43;
-String hola53;
-String hola63;
-String hola73;
-String hola83;
-String hola93;
-String hola56;
-String hola14;
-String hola24;
-String hola34;
-String hola44;
-String hola48;
-String hola64;
-String hola74;
-String hola84;
-String hola94;
-String hola55;
-String hola15;
-String hola25;
-String hola35;
-String hola45;
-String hola95;
-String hola65;
-String hola75;
-String hola85;
-String hola195;
-String hola59;
-String hola16;
-String hola26;
-String hola36;
-String hola46;
-String hola156;
-String hola66;
-String hola76;
-String hola86;
-String hola96;
-void setup() {
-  // put your setup code here, to run once:
+#include <LedControl.h> //Librería para el control de las matrices
 
+
+
+//#####*****************************DEFINICIÓN DE VARIABLES GLOBALES*****************************#####
+
+
+
+
+LedControl matrix = LedControl(12, 11, 10, 4); //Variable para controlar la matrices
+int vel_scroll = 0;
+
+
+
+//#####*****************************DEFINICIÓN DE VARIABLES GLOBALES*****************************#####
+
+
+
+
+
+//####****************************VARIABLES PARA RECIBIR DATOS***************************#####
+const byte numChars = 81; //Tamaño de buffer para recibir datos
+char stringRecibido[numChars]; //Array que guarda los datos recibidos en char
+char tempChars[numChars]; //Array backup de stringRecibido porque el separador va eliminado partes del array origina
+char delimitador[] = ","; //Deliminator de los elementos que trae el string desde la APP para separalos luego
+boolean newData = false; //Booleano que indica si hay nuevos datos para leer desde el serial
+char myStrings[8]; //Char que guardará cada fila del array de bytes para pintar cada estación
+char copia[9]; //Otra copia, de momento esta en desuso
+//#####*****************************VARIABLES PARA RECIBIR DATOS*****************************#####
+
+
+
+
+
+
+
+//#####*****************************INICIALIZACION DE ARDUINO*****************************#####
+
+void setup() {
+  Serial.begin(9600);
+  inicializarMatriz();
 }
+
+
+
+//#####*****************************INICIALIZACION DE ARDUINO*****************************#####
+
+
+
+
+//#####*****************************LOOP PRINCIPAL*****************************#####
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //letrero(); //Letrero mientras no se usan las matrices
+
+  
+    lecturaSerial();//Leyendo la entrada serial
+    if (newData == true) {
+      Serial.println("Nuevos datos encontrados");
+        strcpy(tempChars, stringRecibido);
+            // Hacer una copia de seguridad de los datos
+        parseData(); //Convirtiendo datos de entrada
+        newData = false;
+    }
+  
 
 }
+
+//#####*****************************LOOP PRINCIPAL*****************************#####
+
+
+
+//#####********************************LECTURA SERIAL E IMPRESIÓN DE ESTACIONES********************************#####
+
+
+void lecturaSerial() { //Método que se encarga de leer las entradas en string
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMark = '<';
+  char endMark = '>';
+  char rc;
+  //Serial.println("Leyendo serial");
+  while (Serial.available() > 0 && newData == false) {
+    Serial.println("Leyendo serial");
+    rc = Serial.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMark) {
+        stringRecibido[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        stringRecibido[ndx] = '\0'; // Termino el string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == startMark) {
+      recvInProgress = true;
+    }
+  }
+}
+
+
+
+void parseData() {      //Separar char, convertirlo a string y pintar la matriz
+  int i = 0;
+  char * index; 
+
+  index = strtok(tempChars, delimitador);
+  String s = "";
+  while (index != NULL) {
+    myStrings[i] = index;
+    s=index;
+    s = s.substring(0, s.length()-1);
+    for (int j=0;j<s.length();j++){
+      Serial.println("Esta en el for");
+      Serial.println("Evaluando");
+      Serial.println(s);
+      Serial.println("caracter");
+      Serial.println(s.charAt(j));
+      if ((int)s.charAt(j)=='1'){
+        Serial.println("Es igual");
+        matrix.setLed(1,i,j,true);
+      }
+    }
+    index = strtok(NULL, delimitador);
+    i++;   
+    delay(100);
+  }
+  delay(10000);  
+  for (int i = 0; i < sizeof(myStrings); i++) {
+    Serial.println("En el for");
+    //Serial.println(myStrings[i]);
+    delay(500);
+    if(myStrings[i] == '\0') {
+      Serial.println("Ya no hay mas informacion");
+      }
+  }
+
+}
+
+
+//#####********************************LECTURA SERIAL E IMPRESIÓN DE ESTACIONES********************************#####
+
+
+
+
+
+
+
+//#####********************************INICIALIZAR MATRIZ********************************#####
+
+//Inicialización de la matriz
+//Obtener cantidad de matrices
+//Cancelar modo ahorro
+void inicializarMatriz() {
+  for (int i = 0; i < 4; i++) {
+    matrix.shutdown(i, false);
+    //Configurar brillo
+    matrix.setIntensity(i, 8);
+    //Limpiar la matriz
+    matrix.clearDisplay(i);
+  }
+}
+
+
+
+
+
+//#####********************************INICIALIZAR MATRIZ********************************#####
+
+
+
+
+//#####********************************LETRERO MIENTRAS NO SE USAN LAS ESTACIONES********************************#####
+void letrero() { //Método para mostrar el letrero
+  mensajeScroll(1);
+
+
+}
+
+const PROGMEM bool Message[8][104] = { //Mensaje a mostrar
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+void mensajeScroll(int estado) { //Scroll del texto de izquierda a derecha
+  [&] {
+    for (int d = 0; d < sizeof(Message[0]) - 7; d++) { 
+      for (int col = 0; col < 8; col++) {
+
+        for (int row = 0; row < 8; row++) {
+
+          matrix.setLed(0, row, col, pgm_read_byte(&(Message[row + 1][col + 2 - d])));
+          matrix.setLed(1, row, col, pgm_read_byte(&(Message[row + 1][col + 2 - d])));
+          matrix.setLed(2, row, col, pgm_read_byte(&(Message[row + 1][col + 2 - d])));//****
+        }
+      }
+    }
+  }();
+  matrix.clearDisplay(0);
+  matrix.clearDisplay(1);
+}
+
+//#####********************************LETRERO MIENTRAS NO SE USAN LAS ESTACIONES********************************#####
